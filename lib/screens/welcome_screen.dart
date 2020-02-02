1d572e148b4ce58d3b04408b1d7ae3f29dd84e5c
Rakshana <rakshana.jp@gmail.com>
2020-02-02 17:59:52 +0530

    pushing base code before branching

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -45,11 +45,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               borderRadius: BorderRadius.circular(30.0),
               child: MaterialButton(
                 onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context){
-                      return LoginScreen();
-                    }
-                  ));
+                  Navigator.pushNamed(context, LoginScreen.id);
                 },
                 minWidth: 200.0,
                 height: 42.0,
@@ -67,9 +63,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               color: Colors.brown.shade900,
               child: MaterialButton(
                 onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return RegistrationScreen();
-                  }));
+                  Navigator.pushNamed(context, RegistrationScreen.id);
                 },
                 minWidth: 200.0,
                 height: 42.0,
